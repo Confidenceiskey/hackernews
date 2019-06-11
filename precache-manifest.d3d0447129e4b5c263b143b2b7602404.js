957854173a21e6c3111971581e16c82e9f6e75f0
David N <bigair_1080@hotmail.com>
2019-06-11 15:16:08 -0300

    Updates

---
+++ precache-manifest.d3d0447129e4b5c263b143b2b7602404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f2d61bf8ce8d4795e40449307d5575",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "fbccd5a137fa9ff7a7f2",
+    "url": "/hackernews/static/css/main.8cafaffc.chunk.css"
+  },
+  {
+    "revision": "0f04152813573291918a",
+    "url": "/hackernews/static/js/2.f9e77c39.chunk.js"
+  },
+  {
+    "revision": "fbccd5a137fa9ff7a7f2",
+    "url": "/hackernews/static/js/main.c8dcb240.chunk.js"
+  },
+  {
+    "revision": "c3e44c618c8e72ba32da",
+    "url": "/hackernews/static/js/runtime~main.a6230328.js"
+  }
+]);
